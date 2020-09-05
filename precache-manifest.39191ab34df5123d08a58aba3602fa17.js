9a47ce34cf3b550f234d9a5624fb9cd35f667e24
unknown <you@example.com>
2020-09-05 20:56:24 +0700

    Updates

---
+++ precache-manifest.39191ab34df5123d08a58aba3602fa17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90255e6def136e19f4c7ba17336dc0c1",
+    "url": "/Monsters-gaga/index.html"
+  },
+  {
+    "revision": "4bba3b298b9223d6413b",
+    "url": "/Monsters-gaga/static/css/main.1fea3a8c.chunk.css"
+  },
+  {
+    "revision": "9dadd1a5607abc36395e",
+    "url": "/Monsters-gaga/static/js/2.db3844ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-gaga/static/js/2.db3844ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bba3b298b9223d6413b",
+    "url": "/Monsters-gaga/static/js/main.4fe0fa5a.chunk.js"
+  },
+  {
+    "revision": "3f4eec551317a93dfa8d",
+    "url": "/Monsters-gaga/static/js/runtime-main.11b75f66.js"
+  }
+]);
